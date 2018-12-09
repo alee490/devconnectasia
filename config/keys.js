a6603044fb8911736e53ee956c6b8ffcdb0688b1
Alex Lee <alee490@my.bcit.ca>
2018-12-09 09:13:58 -0800

    Dev/Prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-	mongoURI: 'mongodb://alex:alex123@ds225294.mlab.com:25294/devconnector',
-	secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
